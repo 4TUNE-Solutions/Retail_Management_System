a3a36d91393ca4c76eeeebcb66f2442d95a7fc2e
Lemi5B <61760592+Lemi5B@users.noreply.github.com>
2021-01-13 23:02:54 +0100

    #3 feat Make tables assigned by issue

---
+++ RMSData/dbo/Tables/INV_procurement.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[INV_procurement]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Suplier_id] INT NOT NULL,
+	[Inventory_id] INT NOT NULL,
+	[Creation_date] DATE NOT NULL,
+	[Realized] BIT NOT NULL
+)
---
+++ RMSData/dbo/Tables/INV_procurement_items.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[INV_procurement_items]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Procurement_id] INT NOT NULL,
+	[Article_id] INT NOT NULL,
+	[Amount_number] FLOAT NOT NULL
+)
