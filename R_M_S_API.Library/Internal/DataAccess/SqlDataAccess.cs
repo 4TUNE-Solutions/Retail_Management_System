6f3212ecb005b0031e08777ac883d2422fcd38cb
Aleksej Komnenovic <main@komnen.xyz>
2021-01-13 17:10:25 +0100

    feat Make Class Library for API

---
+++ R_M_S_API.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace R_M_S_API.Library.Internal.DataAccess
+{
+    class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return "x";
+        }
+    }
+}
