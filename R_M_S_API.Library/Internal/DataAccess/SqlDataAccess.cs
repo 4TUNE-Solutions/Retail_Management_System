760d23e1d8cb5e523fd756ac649eecd420590512
Aleksej Komnenovic <main@komnen.xyz>
2021-01-13 18:27:59 +0100

    feat Set up class to manage connection, input to DB and output from database

--- R_M_S_API.Library/Internal/DataAccess/SqlDataAccess.cs
+++ R_M_S_API.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
 using System.Text;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 
 namespace R_M_S_API.Library.Internal.DataAccess
 {
-    class SqlDataAccess
+    public class SqlDataAccess
     {
+        private string _connectionString = string.Empty;
+
         public string GetConnectionString(string name)
         {
-            return "x";
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            var root = configurationBuilder.Build();
+            _connectionString = root.GetSection("ConnectionString").GetSection(name).Value;
+            var appSetting = root.GetSection("ApplicationSettings");
+
+            return _connectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
         }
     }
 }
+
