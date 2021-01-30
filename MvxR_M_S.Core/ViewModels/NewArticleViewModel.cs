f367cccb0a3458ad55db4f22e0dcd4fc526be99b
Aleksej Komnenovic <main@komnen.xyz>
2021-01-31 00:04:13 +0100

    feat Implement 'Go Back' functionality for new VM

---
+++ MvxR_M_S.Core/ViewModels/NewArticleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace MvxR_M_S.Core.ViewModels
+{
+    public class NewArticleViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+        public IMvxAsyncCommand GoBackCommand => new MvxAsyncCommand(GoBack);
+
+        public NewArticleViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        private async Task GoBack()
+        {
+            await _navigationService.Close(this);
+        }
+    }
+}
