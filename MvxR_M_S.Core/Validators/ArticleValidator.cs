79ecb356214976364704b570153618e24a27d592
Aleksej Komnenovic <main@komnen.xyz>
2021-02-02 19:20:27 +0100

    feat Make Validator class and implement logic

---
+++ MvxR_M_S.Core/Validators/ArticleValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MvxR_M_S.Core.Models;
+
+namespace MvxR_M_S.Wpf.Validators
+{
+    public class ArticleValidator : AbstractValidator<ArticleModel>
+    {
+        public ArticleValidator()
+        {
+            RuleFor(a => a.ArticleName)
+                .NotEmpty().WithMessage("Article name can not is empty!");
+
+            RuleFor(a => a.TaxId)
+                .NotEmpty().WithMessage("Tax Id can not be empty!");
+        }
+    }
+}
