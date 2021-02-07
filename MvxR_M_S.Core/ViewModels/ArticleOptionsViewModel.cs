d41a9e614c8eeafe3354f36051352e8c8dbb8dd7
Aleksej Komnenovic <main@komnen.xyz>
2021-02-07 20:22:43 +0100

    feat Make new VVM pair responsible for listing all possible operations within article config group

---
+++ MvxR_M_S.Core/ViewModels/ArticleOptionsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace MvxR_M_S.Core.ViewModels
+{
+    class ArticleOptionsViewModel : MvxViewModel
+    {
+        //navigation backing field
+        private readonly IMvxNavigationService _navigationService;
+
+        public ArticleOptionsViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            ShowArticlesNavigationCommand =
+                new MvxAsyncCommand(() => _navigationService.Navigate<ArticlePresentationViewModel>());
+            InputArticlesNavigationCommand =
+                new MvxAsyncCommand(() => _navigationService.Navigate<NewArticleViewModel>());
+        }
+
+        //command properties
+        public IMvxAsyncCommand GoBackCommand => new MvxAsyncCommand(GoBack);
+        
+        public IMvxAsyncCommand ShowArticlesNavigationCommand { get; private set; }
+
+        public IMvxAsyncCommand InputArticlesNavigationCommand { get; private set; }
+
+        private async Task GoBack()
+        {
+            await _navigationService.Close(this);
+        }
+    }
+}
