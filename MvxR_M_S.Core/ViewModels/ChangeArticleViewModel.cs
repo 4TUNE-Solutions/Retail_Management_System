dd16b01e1c2c5bc5057a05ec627a9fd5d56fb102
Aleksej Komnenovic <main@komnen.xyz>
2021-02-13 17:34:56 +0100

    feat Create View Model for API_PUT UI

---
+++ MvxR_M_S.Core/ViewModels/ChangeArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+
+namespace MvxR_M_S.Core.ViewModels
+{
+    class ChangeArticleViewModel : MvxViewModel
+    {
+
+    }
+}
