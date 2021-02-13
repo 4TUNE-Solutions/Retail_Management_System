13ec04df7fa1cabe0b286c8e1d7543b9ebfe8767
Aleksej Komnenovic <main@komnen.xyz>
2021-02-13 17:42:15 +0100

    feat Set Go Back command

--- MvxR_M_S.Core/ViewModels/ChangeArticleViewModel.cs
+++ MvxR_M_S.Core/ViewModels/ChangeArticleViewModel.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
+using MvxR_M_S.Core.API;
+using MvxR_M_S.Core.Models;
 
 namespace MvxR_M_S.Core.ViewModels
 {
     class ChangeArticleViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
 
+        public ChangeArticleViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public IMvxAsyncCommand GoBackCommand => new MvxAsyncCommand(GoBack);
+
+        public async Task GoBack()
+        {
+            await _navigationService.Close(this);
+        }
     }
 }
