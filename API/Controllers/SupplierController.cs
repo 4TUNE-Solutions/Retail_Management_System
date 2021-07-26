ae50d8e14a401bc524f319c2a32ed8bd010b40a2
Marko Mitrovic <marecare006@gmail.com>
2021-07-26 21:09:48 +0200

    Created an API controller for Suppliers
    Created an API controller for suppliers

---
+++ API/Controllers/SupplierController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Library.DataAccess;
+using API.Library.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        // GET: api/<SupplierController>
+        [HttpGet]
+        public List<SupplierModel> Get()
+        {
+            var data = new SupplierData();
+
+            return data.GetSuppliers();
+        }
+
+        // GET api/<SupplierController>/5
+        [HttpGet("{id}")]
+        public SupplierModel Get(int id)
+        {
+            var data = new SupplierData();
+
+            return data.GetSupplierById(id);
+        }
+
+        // POST api/<SupplierController>
+        [HttpPost]
+        public void Post(SupplierModel supplier)
+        {
+            var data = new SupplierData();
+            data.SetSupplier<SupplierModel>(supplier);
+        }
+
+        // PUT api/<SupplierController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, SupplierModel am)
+        {
+            var data = new SupplierData();
+
+            data.ChangeSupplierById<SupplierModel>(am);
+        }
+
+        // DELETE api/<SupplierController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var data = new SupplierData();
+
+            data.DeleteSupplierById(id);
+        }
+    }
+}
