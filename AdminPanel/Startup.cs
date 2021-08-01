6890d26446437ea74c63f6d5a26060bf3ca57449
Aleksej Komnenovic <azure@komnen.xyz>
2021-08-01 17:46:01 +0200

    add using commands for AuthN and AuthZ

--- AdminPanel/Startup.cs
+++ AdminPanel/Startup.cs
@@ -55,8 +55,12 @@ namespace AdminPanel
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
