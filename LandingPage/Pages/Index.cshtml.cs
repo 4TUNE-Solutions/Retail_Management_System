d0c4dbe39b8068675d4f6f4cfcaa47afdcd28783
Aleksej Komnenovic <azure@komnen.xyz>
2021-08-12 13:21:15 +0200

    set redirects to wordpress landing

--- LandingPage/Pages/Index.cshtml.cs
+++ LandingPage/Pages/Index.cshtml.cs
@@ -17,9 +17,11 @@ namespace LandingPage.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            string url = "https://asiss.azurewebsites.net";
 
+            return Redirect(url);
         }
     }
 }
